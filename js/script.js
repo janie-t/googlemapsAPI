d0777aef62df227060d4bc5ea6573acba4c5f50b
Janie Tito <jtito@hotmail.com>
2016-11-13 19:40:28 +1300

    got NYtimes article grab to work

--- js/script.js
+++ js/script.js
@@ -21,6 +21,24 @@ function loadData() {
     var streetView = 'https://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + address;
     $body.append('<img class = "bgimg" src = "' + streetView +'">');
 
+    //get the articles from the NY Times -first create the URL variable for later use
+    var nytimesURL = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + city + '&sort=newest&api-key=f50bb26e6a854d099db476f658d98dc2'
+
+    //this anonymous function will run as soon as the data from the NYtimes is returned.
+    $.getJSON(nytimesURL, function(data){
+
+      //Insert some extra text into the nytHeaderElem section
+      $nytHeaderElem.text('New York Times Articles About ' + city);
+
+      /*the next section goes through each article that is returned, and takes some text from it, such as the headline, and puts that in our html doc.*/
+      articles = data.response.docs;
+      for(var i=0; i<articles.length; i++){
+        var article = articles[i];
+        $nytElem.append('<li class="article">'+'<a href="'+article.web_url+'">'+article.headline.main+'</a>'+'<p>'+ article.snippet + '</p>'+ '</li>');
+      };
+
+    })
+
     return false;
 };
 
